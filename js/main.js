d2a871f3e0b827f6e96b94b15f40fcd076cd773a
Colin Fenn <colin.s.fenn@gmail.com>
2021-04-01 04:17:09 -0700

    Corrected formulas. Added accurate #s for SaturnV.

--- js/main.js
+++ js/main.js
@@ -1,25 +1,33 @@
 var json = {
     "Vehicles": [
+        // Mass is in kg. Velocities are in kms^-1
         {
             "id": "1",
-            "name":"Vehicle 1",
-            "speed":"50",
-            "weight":"20"
+            "name": "Saturn V",
+            "stage 1 wet mass": 2290000,
+            "stage 1 dry mass": 130000,
+            "stage 1 exhaust velocity": 2.58,
+            "stage 2 wet mass": 496200,
+            "stage 2 dry mass": 40100,
+            "stage 2 exhaust velocity": 4.13,
+            "stage 3 wet mass": 123000,
+            "stage 3 dry mass": 13500,
+            "stage 3 exhaust velocity": 4.13
         },
         {
             "id": "2",
-            "name":"Vehicle 2",
-            "speed":"70",
-            "weight":"10"
+            "name": "Falcon Heavy",
+            "speed": "70",
+            "weight": "10"
         },
         {
             "id": "3",
-            "name":"Vehicle 3",
-            "speed":"20",
-            "weight":"90"
+            "name": "Falcon 9",
+            "speed": "20",
+            "weight": "90"
         }
-    ], 
-    "Destinations":[
+    ],
+    "Destinations": [
         {
             "id": "1",
             "name": "pluto",
@@ -44,40 +52,39 @@ var state = {
     "weight": ""
 }
 
-
 var form = document.getElementById("delta_v");
 var v_ele = document.getElementById("vehicle");
 var d_ele = document.getElementById("destination");
 var w_ele = document.getElementById("weight")
 var output = document.getElementById("output")
 
-form.addEventListener("submit", function(e) {
+form.addEventListener("submit", function (e) {
     getFormVals();
     e.preventDefault();
 })
 
-v_ele.addEventListener("change", function(e){
+v_ele.addEventListener("change", function (e) {
     setVehicleID(e)
 })
 
-v_ele.addEventListener("change", function(e){
+v_ele.addEventListener("change", function (e) {
     setDestinationID(e)
 })
 
-w_ele.addEventListener("change", function(e) {
+w_ele.addEventListener("change", function (e) {
     console.log(e)
     setWeightVal(e)
 })
 
-function setVehicleID(e){
+function setVehicleID(e) {
     state.vehicle_id = e.target.options[e.target.options.selectedIndex].value;
 }
 
-function setDestinationID(e){
+function setDestinationID(e) {
     state.destination_id = e.target.options[e.target.options.selectedIndex].value;
 }
 
-function setWeightVal(e){
+function setWeightVal(e) {
     state.weight = e.target.value;
 }
 
@@ -87,29 +94,27 @@ function getFormVals() {
     let destination = findDestination()
     // If either is null then set output to error
 
-    if(vehicle === null){
+    if (vehicle === null) {
         Out("Not a valid vehicle")
         return;
     }
-    
-    if(destination === null){
+    if (destination === null) {
         Out("Not a valid destination")
         return;
     }
-    
-    if(Number.isNaN(state.weight)){
+    if (Number.isNaN(state.weight)) {
         Out("Not a valid number")
         return;
     }
-    //Bogus function for funsies
-    Out((destination.distance*state.weight)/vehicle.speed)
+    //Delta V = Vehicle Exaust Velocity * Log(wet mass/dry mass)
+    Out((vehicle["stage 1 exhaust velocity"] * math.log(vehicle["stage 1 wet mass"] / vehicle["stage 1 dry mass"]) + (vehicle["stage 2 exhaust velocity"] * math.log(vehicle["stage 2 wet mass"] / vehicle["stage 2 dry mass"]) + (vehicle["stage 3 exhaust velocity"] * math.log(vehicle["stage 3 wet mass"] / vehicle["stage 3 dry mass"]))
+    // Above formula SHOULD be printing out exactly 12.97 at the moment if you select "Saturn V". This script will be working when I see that number. 
+    
+    }
 
-
-}
-
-function findVehicle(){
-    for (i in json.Vehicles){
-        if(json.Vehicles[i].id === state.vehicle_id){
+function findVehicle() {
+    for (i in json.Vehicles) {
+        if (json.Vehicles[i].id === state.vehicle_id) {
             console.log("Got Vehicle")
             return json.Vehicles[i]
         }
@@ -117,9 +122,9 @@ function findVehicle(){
     return null
 }
 
-function findDestination(){
-    for (i in json.Destinations){
-        if(json.Destinations[i].id === state.destination_id){
+function findDestination() {
+    for (i in json.Destinations) {
+        if (json.Destinations[i].id === state.destination_id) {
             console.log("Got Destination")
             return json.Destinations[i]
         }
@@ -127,6 +132,6 @@ function findDestination(){
     return null
 }
 
-function Out(message){
+function Out(message) {
     output.value = message;
 }
